f496ca0cdc664b66cbd7eaf37b66a41d6652f108
Kittaka <jdavisson87@gmail.com>
2021-06-06 12:17:23 -0500

    added pre-work comments

--- src/polybius.js
+++ src/polybius.js
@@ -6,9 +6,39 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
-  function polybius(input, encode = true) {
+  // takes in a string message
+  const convertPolybiusWordEncode = (word) => {
+    // create result array
+    // separate word into an array of letters
+    // match letter with coordinate number
+    // push coordinate to a result array
+    // join result array
+    // return result array that is a string of numbers
+  };
+
+  // takes in a string of numbers.
+  const convertPolybiusWordDecode = (word) => {
+    // create result array
+    // separate argument into single numbers and save in a temp array
+    // run through the temp array with for loop that iterates every other index.
+    // in this loop, the pointer and the next item will be used to locate the letter
+    // push letter into the result array
+    // join result array
+    // return result array
+  };
+
+  // cypher where each letter is represented by a coordinate
+  // arguments include an input and whether we are encoding or decoding
+  // returns a string of numbers or the decoded message
+  const polybius = (input, encode = true) => {
     // your solution code here
-  }
+    // be sure input is in lower case
+    // create an array with the input separated by ' '
+    // if encode===true, run convertPolybiusWordEncode on each word
+    // if encode===false, run convertPolybiusWordDecode on each word
+    // join words with ' '
+    // return a string
+  };
 
   return {
     polybius,
