5bde19e4018462d54e9410b85f53300eda17b398
Kittaka <jdavisson87@gmail.com>
2021-06-05 21:41:16 -0500

    First test for Caesar function.  Returns false if shift is under -25

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -1,1 +1,9 @@
-// Write your tests here!
+const { caesar } = require('../src/caesar.js');
+const expect = require('chai').expect;
+
+describe('caesar', () => {
+  it('returns false when shift value is less than -25', () => {
+    const actual = caesar('thinkful', -30);
+    expect(actual).to.be.false;
+  });
+});
