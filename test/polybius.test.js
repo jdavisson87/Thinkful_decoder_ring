9b4cb0f9c327b64f6dfff284fe9fc4be343ad474
Kittaka <jdavisson87@gmail.com>
2021-06-06 13:25:12 -0500

    created tests for helper functions

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -8,6 +8,22 @@ const expect = require('chai').expect;
 
 describe('polybius', () => {});
 
-describe('convertPolybiusEncode', () => {});
+describe('convertPolybiusEncode', () => {
+  it('encodes word from letters to numbers', () => {
+    const actual = convertPolybiusWordEncode('thinkful');
+    const expected = '4432423352125413';
+    expect(actual).to.equal(expected);
+  });
+});
 
-describe('convertPolybiusWordDecode', () => {});
+describe('convertPolybiusWordDecode', () => {
+  it('decodes word from numbers to letters', () => {
+    const actual = convertPolybiusWordDecode('4432423352125413');
+    const expected = 'th(i/j)nkful';
+    expect(actual).to.equal(expected);
+  });
+  it('returns false if argument length is odd', () => {
+    const actual = convertPolybiusWordDecode('443242335212541');
+    expect(actual).to.be.false;
+  });
+});
