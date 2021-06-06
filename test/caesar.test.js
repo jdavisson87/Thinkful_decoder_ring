f63f5c65a4dcd89924f27516c8d15461af3f934c
Kittaka <jdavisson87@gmail.com>
2021-06-05 21:46:18 -0500

    tests for validating the shift value

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -3,7 +3,19 @@ const expect = require('chai').expect;
 
 describe('caesar', () => {
   it('returns false when shift value is less than -25', () => {
-    const actual = caesar('thinkful', -30);
+    const actual = caesar('thinkful', -26);
+    expect(actual).to.be.false;
+  });
+  it('returns false when shift value is greater than 25', () => {
+    const actual = caesar('thinkful', 26);
+    expect(actual).to.be.false;
+  });
+  it('returns false when shift value is 0', () => {
+    const actual = caesar('thinkful', 0);
+    expect(actual).to.be.false;
+  });
+  it('returns false if no shift value is present', () => {
+    const actual = caesar('thinkful');
     expect(actual).to.be.false;
   });
 });
