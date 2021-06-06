5fde7bcc32afa8106f374916c53c350427ccda3a
Kittaka <jdavisson87@gmail.com>
2021-06-06 12:22:01 -0500

    created skeleton of tests

--- test/polybius.test.js
+++ test/polybius.test.js
@@ -1,1 +1,13 @@
 // Write your tests here!
+const {
+  polybius,
+  convertPolybiusWordEncode,
+  convertPolybiusWordDecode,
+} = require('../src/polybius');
+const expect = require('chai').expect;
+
+describe('polybius', () => {});
+
+describe('convertPolybiusEncode', () => {});
+
+describe('convertPolybiusWordDecode', () => {});
