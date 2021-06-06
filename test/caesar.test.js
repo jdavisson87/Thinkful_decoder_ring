fb2360030d65aa2af2fe4dc2c560e4eaa22a8025
Kittaka <jdavisson87@gmail.com>
2021-06-06 11:01:29 -0500

    tests encoding

--- test/caesar.test.js
+++ test/caesar.test.js
@@ -18,4 +18,19 @@ describe('caesar', () => {
     const actual = caesar('thinkful');
     expect(actual).to.be.false;
   });
+  it('should encode a message', () => {
+    const actual = caesar('thinkful', 3);
+    const expected = 'wklqnixo';
+    expect(actual).to.equal(expected);
+  });
+  it('should handle a negative shift value', () => {
+    const actual = caesar('thinkful', -3);
+    const expected = 'qefkhcri';
+    expect(actual).to.equal(expected);
+  });
+  it('should handle letters at the beginning of the alphabet with a negative shift value', () => {
+    const actual = caesar('a', -3);
+    const expected = 'x';
+    expect(actual).to.equal(expected);
+  });
 });
