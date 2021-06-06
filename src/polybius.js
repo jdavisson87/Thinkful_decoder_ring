f98f62ac28fc66c20bdeeb06cb4e114d063cb984
Kittaka <jdavisson87@gmail.com>
2021-06-06 12:32:26 -0500

    created encode and decode cypher

--- src/polybius.js
+++ src/polybius.js
@@ -6,6 +6,45 @@
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
 
+  // encode cypher
+  const encodeCypher = {
+    a: '11',
+    b: '21',
+    c: '31',
+    d: '41',
+    e: '51',
+    f: '12',
+    g: '22',
+    h: '32',
+    i: '42',
+    j: '42',
+    k: '52',
+    l: '13',
+    m: '23',
+    n: '33',
+    o: '43',
+    p: '53',
+    q: '14',
+    r: '24',
+    s: '34',
+    t: '44',
+    u: '54',
+    v: '15',
+    w: '25',
+    x: '35',
+    y: '45',
+    z: '55',
+  };
+
+  // decode cypher
+  const decodeCypher = {
+    1: { 1: 'a', 2: 'f', 3: 'l', 4: 'q', 5: 'v' },
+    2: { 1: 'b', 2: 'g', 3: 'm', 4: 'r', 5: 'w' },
+    3: { 1: 'c', 2: 'h', 3: 'n', 4: 's', 5: 'x' },
+    4: { 1: 'd', 2: '(i/j)', 3: 'o', 4: 't', 5: 'y' },
+    5: { 1: 'e', 2: 'k', 3: 'p', 4: 'u', 5: 'z' },
+  };
+
   // takes in a string message
   const convertPolybiusWordEncode = (word) => {
     // create result array
